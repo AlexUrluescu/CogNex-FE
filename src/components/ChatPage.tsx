a876223e6d71755272cb8db28803397042b66b1a
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-02-18 17:55:11 +0200

    get the link fo the pdf

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Chat from "../components/Chat";
 
 const ChatPage = () => {
+  const [pdfUrl, setPdfUrl] = useState<any>(null);
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {};
 
   const url = process.env.NEXT_PUBLIC_ROUTE;
@@ -27,16 +28,28 @@ const ChatPage = () => {
   };
 
   const handleClick = async () => {
-    const res = await fetch("http://127.0.0.1:5000/test");
-    const data = await res.json();
+    try {
+      const response = await fetch("http://127.0.0.1:5000/pdfs/ro_test.pdf");
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      console.log("url", url);
 
-    console.log("data", data);
+      setPdfUrl(url);
+    } catch (error) {
+      console.error("Error fetching PDF:", error);
+    }
   };
 
   return (
     <div>
       <div>
         <h1>CogNex</h1>
+        {pdfUrl ? (
+          <a href={pdfUrl} target="_blank" rel="noopener noreferrer">
+            View PDF
+          </a>
+        ) : null}
+
         <button onClick={handleClick}>test</button>
         <input type="text" onChange={handleChange}></input>
         <button onClick={handleSend}>send</button>
