5eff7e4bfb7521b7ffb2a5cefd14a90b4b5ffc4c
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-04-03 21:24:11 +0300

    work into the design and arhitecture

---
+++ src/pages/my-knowledge/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/layout'
+import { MyKnowledgeView } from '@/views/my-knowledge'
+import React from 'react'
+
+const MyKnowledge = () => {
+  return (
+    <Layout>
+      <MyKnowledgeView />
+    </Layout>
+  )
+}
+
+export default MyKnowledge
