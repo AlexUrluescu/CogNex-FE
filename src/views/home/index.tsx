a8db595834b789e3f7b5e621b7026c75dcdca8c4
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-04-03 21:23:26 +0300

    create the views of each route

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,62 +7,16 @@ import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
 import { CustomMenu } from '@/components/Menu'
 import { useRouter } from 'next/router'
+import Header from '@/layout/components/header'
+import Layout from '@/layout'
 
 const HomeView = () => {
   const currentUser = useSelector(getCurrentUser)
 
-  const router = useRouter()
-
-  const items: MenuProps['items'] = [
-    {
-      label: <Flex gap={10}>My account</Flex>,
-      key: '0',
-    },
-    {
-      type: 'divider',
-    },
-    {
-      label:
-        currentUser !== undefined ? (
-          <Button type="primary" onClick={() => router.push('/register')}>
-            Log out
-          </Button>
-        ) : (
-          'Log in'
-        ),
-      key: '3',
-    },
-  ]
-
   return (
-    <div>
-      <Flex
-        justify="space-between"
-        align="center"
-        style={{ backgroundColor: '#02182B', padding: '20px 30px', color: 'white' }}
-      >
-        <h2>Teleport</h2>
-        <span>
-          <Dropdown
-            menu={{ items }}
-            placement="bottom"
-            arrow={{ pointAtCenter: true }}
-            trigger={['click']}
-          >
-            <Flex style={{ cursor: 'pointer' }} gap={10} onClick={(e) => e.preventDefault()}>
-              <UserOutlined />
-              {currentUser.firstName} {currentUser.lastName}
-            </Flex>
-          </Dropdown>
-        </span>
-      </Flex>
-      <Flex style={{ width: '100%' }}>
-        <Flex vertical style={{ backgroundColor: 'blue', width: '20%' }}>
-          <CustomMenu currentUserId={currentUser._id} />
-        </Flex>
-        <ChatPage />
-      </Flex>
-    </div>
+    <Layout>
+      <ChatPage />
+    </Layout>
   )
 }
 
