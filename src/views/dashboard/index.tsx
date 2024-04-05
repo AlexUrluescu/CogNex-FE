919964364457c250b6430f9252757afb56ad7b57
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-04-05 14:19:59 +0300

    fix the fetching method in the app

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,5 +1,16 @@
+import { getChatsAsCreator, getCurrentUser } from '@/state/appData/selectors'
+import { Button, Card, Flex } from 'antd'
+import { useRouter } from 'next/router'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const DashboardView = () => {
-  return <div>DashboardView</div>
+  const currentUser = useSelector(getCurrentUser)
+  const router = useRouter()
+
+  return (
+    <Flex vertical gap={50}>
+      <h1>Dashboard</h1>
+    </Flex>
+  )
 }
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,23 +1,8 @@
 import ChatPage from '@/components/ChatPage'
-import { getCurrentUser } from '@/state/appData/selectors'
-import { Button, Dropdown, Flex, Space } from 'antd'
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons'
-import type { MenuProps } from 'antd'
-import { CustomMenu } from '@/components/Menu'
-import { useRouter } from 'next/router'
-import Header from '@/layout/components/header'
-import Layout from '@/layout'
 
 const HomeView = () => {
-  const currentUser = useSelector(getCurrentUser)
-
-  return (
-    <Layout>
-      <ChatPage />
-    </Layout>
-  )
+  return <ChatPage />
 }
 
 export default HomeView
